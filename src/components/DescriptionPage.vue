5421f1f9aefde44307bfd3de0a04e5187fa3a862
Muska_Ami <xmcn@1l1.icu>
2024-06-23 07:30:58 +0800

    随便改了一下

--- src/components/DescriptionPage.vue
+++ src/components/DescriptionPage.vue
@@ -19,6 +19,18 @@
                 <mdui-button style="background-color: #66ccff;">文档中心</mdui-button>
             </a>
         </p>
+        <p>
+            <br>
+            风雨阁 Storm Pavilion 保留所有权利。<br>
+            "Minecraft"及其产品为 Mojang AB 的商标。本服务器(组织)不隶属于 Mojang AB。<br>
+            本服务器在遵守 <a href="https://www.minecraft.net/eula" target="_blank">《Minecraft EULA》(最终用户许可协议) </a>的情况下运营。<br>
+            本服务器不包含任何内购产品。<br>
+            <br>
+        </p>
+
+        <p class="moeicp-description">
+            <a href="https://icp.gov.moe/?keyword=20229960" target="_blank">萌ICP备20229960号</a>
+        </p>
     </div>
 </template>
 
@@ -29,5 +41,15 @@
 
 .buttons mdui-button {
     margin: 0.3rem;
+}
+
+@media screen and (min-width: 700px) {
+    .moeicp-description {
+        display: none;
+    }
+
+    /* .footer {
+        height: 300px;
+    } */
 }
 </style>
--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -3,16 +3,10 @@
         <p>
             Copyright © 2021-{{ nowYear }} 风雨阁Storm Pavilion 版权所有。<br>
             <a href="https://tos-eula.spcraft.cn/" target="_blank">风雨阁 Storm Pavilion 《服务条款及最终用户许可协议》</a><br>
-            <a href="https://icp.gov.moe/?keyword=20229960" target="_blank">萌ICP备20229960号</a>
         </p>
         <div style="flex-grow: 1"></div>
-        <p>
-            <br>
-            风雨阁 Storm Pavilion 保留所有权利。<br>
-            "Minecraft"及其产品为 Mojang AB 的商标。本服务器(组织)不隶属于 Mojang AB。<br>
-            本服务器在遵守 <a href="https://www.minecraft.net/eula" target="_blank">《Minecraft EULA》(最终用户许可协议) </a>的情况下运营。<br>
-            本服务器不包含任何内购产品。<br>
-            <br>
+        <p class="moeicp-right">
+            <a href="https://icp.gov.moe/?keyword=20229960" target="_blank">萌ICP备20229960号</a>
         </p>
     </mdui-bottom-app-bar>
 </template>
@@ -23,12 +17,20 @@ var nowYear = new Date().getFullYear();
 
 <style scoped>
 .footer {
-    height: 150px;
+    font-size: small;
 }
 
+/* .footer {
+    height: 150px;
+} */
+
 @media screen and (max-width: 700px) {
-    .footer {
-        height: 300px;
+    .moeicp-right {
+        display: none;
     }
+
+    /* .footer {
+        height: 300px;
+    } */
 }
 </style>
